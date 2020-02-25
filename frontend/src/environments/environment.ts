2a0d0793dad165bb85696864f34765cf8976d94c
Rafy <rafy.amgadbenjamin@gmail.com>
2020-02-25 16:37:17 +0200

    Adding environmental variables to the front-end

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-8kyz9p1y.auth0.com', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'beBQE8gFaw3pKzgqsesx1M2ZBsPjlwmX', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
